4641436d0fd19ac3bbd84279a7541d685140498f
indhuJothi <indhu.palanivel@aspiresys.com>
2021-08-21 09:04:25 +0530

    safety-commit

--- src/context.js
+++ src/context.js
@@ -1,3 +1,4 @@
-import React from "react"
+import React from "react";
 
-export const userContext = React.createContext({user:" "})
+export const userContext = React.createContext({ password: " " });
+s
